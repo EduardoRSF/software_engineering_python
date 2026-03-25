bd293efea429fa2cdf19a58f7246002b7a45af3b
Rodrigues Silva Filho <Eduardo.RodriguesSilvaFilho@dlr.de>
2026-03-25 14:39:13 +0100

    made test robust to floating point variations

--- test/test_main.py
+++ test/test_main.py
@@ -4,7 +4,7 @@ import numpy as np
 def test_distance():
   r1 = np.array([1,1])
   r2 = np.array([2,2])
-  assert(VicsekModel.distance(r1, r2) == np.sqrt(2))
+  assert(np.isclose(VicsekModel.distance(r1, r2), np.sqrt(2)))
   
 def test_main():
   assert(True)
